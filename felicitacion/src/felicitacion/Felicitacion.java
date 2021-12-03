28ce78fc11ada3655ec6a3b9b29e549c47bd08b6
NachoAS <profesornachoagudo@gmail.com>
2021-12-03 12:11:27 +0100

    primer comit de todo el workspace

---
+++ felicitacion/src/felicitacion/Felicitacion.java
@@ -0,0 +1,19 @@
+/**
+ * Programa para felicitar a mamá
+ */
+package felicitacion;
+
+/**
+ * @author Antonio Cepero
+ *
+ */
+public class Felicitacion {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Felicidades mamá!");
+	}
+
+}
